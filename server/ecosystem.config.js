34f0e658bace42b6c4b11fb95561ed1dc99d65b6
annenicolebarria <canicolebarria@gmail.com>
2025-11-23 13:51:58 +0800

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: "frontend",
       script: "/root/.nvm/versions/node/v18.20.8/lib/node_modules/serve/build/main.js",
-      interpreter: "/root/.nvm/versions/node/v18.20.8/bin/node",
-      args: "-s /root/web/dist -l tcp://0.0.0.0:3000"
+      exec_interpreter: "/root/.nvm/versions/node/v18.20.8/bin/node",
+      args: "-s /root/web/dist -l tcp://0.0.0.0:3000",
+      exec_mode: "fork"
     }
   ]
 };
