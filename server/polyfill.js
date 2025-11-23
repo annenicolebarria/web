f28ecd38bc28ca1f70eb29a4d22831c3df1ee223
annenicolebarria <canicolebarria@gmail.com>
2025-11-23 14:13:30 +0800

    Update polyfill.js

--- server/polyfill.js
+++ server/polyfill.js
@@ -1,3 +1,9 @@
-if (typeof File === 'undefined') {
-  global.File = class File {};
+if (typeof global.File === "undefined") {
+  global.File = class File extends Blob {
+    constructor(parts, filename, options = {}) {
+      super(parts, options);
+      this.name = filename || "";
+      this.lastModified = options.lastModified || Date.now();
+    }
+  };
 }
